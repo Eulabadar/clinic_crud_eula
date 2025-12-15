881d244861ae2c34bbc67a2995f225cba0f86bf7
Eula Yvette Badar <eulayvetteb@gmail.com>
2025-12-16 00:24:36 +0800

    Enable CORS for localhost + netlify

--- app.js
+++ app.js
@@ -24,24 +24,23 @@ const ALLOWED_ORIGINS = [
 app.use((req, res, next) => {
   const origin = req.headers.origin;
 
-  // If request has an Origin header and it's in the allowlist, allow it
-  if (origin && ALLOWED_ORIGINS.includes(origin)) {
+  const allowed = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "https://monstera-6dcf01.netlify.app",
+  ];
+
+  if (origin && allowed.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-  // Always set these:
   res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  // Preflight
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
+  if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
 });
-
 app.use(express.json());
 
 connectDB();
